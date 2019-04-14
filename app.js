62811ab7665b396b8e4e08dfec86d866ad6266d9
Trayvont York <trayvont.york@gmail.com>
2019-04-14 11:59:14 -0400

    Bad Request error in passport.authenticate

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const app = express();
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride("_method"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/RememberMeDB", {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
-    name: String,
     email: String,
-    phone: Number,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userSchema);
+
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 let userList; //list of users in database
 
@@ -44,28 +68,37 @@ app.get("/signup", function(req, res){
     res.render('signup');
 });
 
+app.get("/profile", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("profile");
+    } else {
+        //res.redirect("/login");
+        res.send("Redirected from profile");
+    }
+})
+
 app.post("/signup", function(req, res){
-    const name = req.body.userName;
-    const email = req.body.userEmail;
-    const phone = req.body.userPhone;
-    const password = req.body.userPassword;
-
-    console.log(name, email, phone, password);
-
-    const user = new User({
-        name: name,
-        email: email,
-        phone: phone,
-        password: password
-    });
-
-    user.save();
-
-    res.send("<h1>Thank you for signing up!</h1>");
+    console.log(req.body.userEmail, req.body.userPassword);
+    User.register({username: req.body.userEmail}, req.body.userPassword, function(err, user){
+        console.log("in register");
+        if(err){
+            console.log("error exists");
+            console.log(err);
+            res.redirect('/signup');
+        } else {
+            console.log("in else block");
+            passport.authenticate("local")(req, res, function(){
+                console.log("before redirect");
+                res.redirect('/');
+                console.log("after redirect");
+                //   res.send("User found");
+            });
+            console.log("after authenticate");
+        }
+    })
 });
 
 app.get("/login", function(req, res){
-
     res.render("login");
 });
 
@@ -73,18 +106,17 @@ app.post("/login", function(req, res){
     const email = req.body.userEmail;
     const password = req.body.userPassword;
 
-    User.findOne({email: email, password: password}, function(err, user){
+    User.findOne({email: email}, function(err, foundUser){
         if(err){
             console.log(err);
         } else {
-            if(user){
+            if(foundUser.password === password){
                 res.send("<h1>Found user successfully</h1>");
             } else {
                 res.send("<h1>User was not found</h1>");
             }
         }
     });
-    // res.send("Login Page Results");
 });
 
 app.listen(3000, function(){
