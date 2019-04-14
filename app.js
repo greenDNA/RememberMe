a3bbbc8d27bfe8018da8b6e133b44dbc04c94160
Trayvont York <trayvont.york@gmail.com>
2019-04-13 22:24:17 -0400

    Users can now sign up to create an account

--- app.js
+++ app.js
@@ -13,25 +13,56 @@ app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost:27017/RememberMeDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
-    _id: Number,
     name: String,
     email: String,
+    phone: Number,
     password: String
 });
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/", function(req, res){
-    res.render('home', {});
+let userList; //list of users in database
 
-})
+app.get("/", function(req, res){
+    userList = [];
+    User.find(function(err, users){
+        if(err){
+            console.log(err);
+        } else {
+            users.forEach(user => {
+                //console.log(user);
+                userList.push(user);
+            });
+            console.log(userList);
+            res.render('home', {list: userList});
+        }
+    });
+
+
+});
 
 app.get("/signup", function(req, res){
     res.render('signup');
 });
 
 app.post("/signup", function(req, res){
-    res.send("<h1>Thank you for signing up!</h1>")
+    const name = req.body.userName;
+    const email = req.body.userEmail;
+    const phone = req.body.userPhone;
+    const password = req.body.userPassword;
+
+    console.log(name, email, phone, password);
+
+    const user = new User({
+        name: name,
+        email: email,
+        phone: phone,
+        password: password
+    });
+
+    user.save();
+
+    res.send("<h1>Thank you for signing up!</h1>");
 });
 
 app.listen(3000, function(){
